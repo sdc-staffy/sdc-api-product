d7be1ac60cb2d5769b8f3b30a685a6694bbea939
Neeki Askari <neeki.askari@gmail.com>
2023-05-27 15:27:42 -0400

    get style info API route done

--- server/index.js
+++ server/index.js
@@ -80,16 +80,64 @@ app.get('/products/:product_id', (req, res) => {
 
 //get one product's styles 
 
-/*
+const getStyleInfo = async (product_id) => {
+    const styleInfoQuery = `
+    SELECT json_build_object(
+        'product_id', styles.product_id, 
+        'results', json_agg(
+            json_build_object(
+              'style_id', style_id,
+              'name', name,
+              'original_price', original_price,
+              'sale_price', sale_price,
+              'default?', CASE WHEN default_style::integer = 1 THEN true ELSE false END,
+              'photos', photos_agg,
+              'skus', skus_agg
+          )
+        ) 
+      ) as styles_info
+      FROM styles
+      LEFT JOIN (
+          SELECT styleid, json_agg(
+              json_build_object(
+                  'thumbnail_url', thumbnail_url,
+                  'url', url
+              )) as photos_agg
+          FROM photos
+          GROUP BY styleid
+          ) photos ON styles.style_id = photos.styleid
+      LEFT JOIN (
+          SELECT styleid, json_agg(
+              json_build_object(
+                  sku_id, json_build_object(
+                      'quantity', quantity,
+                      'size', size 
+                  )
+              )
+          ) as skus_agg
+          FROM skus
+          GROUP BY styleid
+      ) skus ON styles.style_id = skus.styleid
+      WHERE styles.product_id = ${product_id}
+      GROUP BY styles.product_id;      
+    `
+    try {
+        const response = await db.query(styleInfoQuery);
+        return response.rows[0].styles_info;
+    } catch (err) {
+        return console.error(err);
+    }
+}
+
 app.get('/products/:product_id/styles', (req, res) => {
-    const product_id = ....
+    const product_id = Number.parseInt(req.params.product_id)
     
-    db.getStyleInfo(product_id)
+    getStyleInfo(product_id)
         .then((data) => {
             res.send(data)
         })
 })
-*/
+
 
 
 //get one product's related products
