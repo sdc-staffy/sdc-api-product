ea295decd70b0fd0f1561897d22d14a256fc3aed
Neeki Askari <neeki.askari@gmail.com>
2023-05-27 12:32:09 -0400

    get one product info route complete

--- server/index.js
+++ server/index.js
@@ -6,19 +6,26 @@ const app = express();
 app.use(express.json());
 
 const getAllProducts = async (count) => {
-    const productQuery = `SELECT json_build_object('id', id, 'name', name, 'slogan', slogan, 'description', description, 'category', category, 'default_price', default_price ) as product_info
-    FROM product
-    GROUP BY id
-    LIMIT ${count};`
-    return db.query(productQuery)
-        .then((response) => {
-            const formatted = [];
-            response.rows.forEach((row) => {
-                formatted.push(row.product_info)
-            })
-            return formatted;
-        })
-        .catch((err) => console.error(err));
+    const productQuery = `SELECT json_build_object(
+        'id', id, 
+        'name', name, 
+        'slogan', slogan, 
+        'description', description, 
+        'category', category, 
+        'default_price', default_price ) as product_info
+        FROM product
+        GROUP BY id
+        LIMIT ${count};`
+    try {
+        const response = await db.query(productQuery);
+        const formatted = [];
+        response.rows.forEach((row) => {
+            formatted.push(row.product_info);
+        });
+        return formatted;
+    } catch (err) {
+        return console.error(err);
+    }
 }
 //get list of all products (default limit 5)
 
@@ -31,20 +38,45 @@ app.get('/products', (req, res) => {
         .catch((err)=>console.log('could not get products:', err))
 })
 
+const getProductInfo = async (product_id) => {
+    const productInfoQuery = `SELECT json_build_object(
+        'id', product.id, 
+        'name', product.name, 
+        'slogan', product.slogan, 
+        'description', product.description, 
+        'category', product.category, 
+        'default_price', product.default_price, 
+        'features', json_agg(
+          json_build_object(
+            'feature', features.feature, 
+            'value', features.value
+          )
+        )
+      ) as product_info
+      FROM product
+      RIGHT JOIN features ON product.id = features.product_id
+      WHERE product.id = ${product_id}
+      GROUP BY product.id;`
+    
+    try {
+        const response = await db.query(productInfoQuery);
+        return response.rows[0].product_info;
+    } catch (err) {
+        return console.error(err);
+    }
+}
 
 //get one product information 
-    //product info + features
 
-/*
 app.get('/products/:product_id', (req, res) => {
-    const product_id = ....
+    const product_id = Number.parseInt(req.params.product_id)
 
-     db.getProductInfo(product_id)
+     getProductInfo(product_id)
         .then((data) => {
             res.send(data)
         })
+        .catch((err)=>console.log('could not get products:', err))
 })
-*/
 
 //get one product's styles 
 
