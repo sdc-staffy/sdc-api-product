4477839214660c962943f5df7d298caed6c54823
Neeki Askari <125829634+Neeki-Askari@users.noreply.github.com>
2023-05-26 21:39:41 -0400

    Update webpack.config.js
    Co-authored-by: Harold Peña <33985296+afrodynamic@users.noreply.github.com>

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
     rules: [
       {
         test: /\.(js|jsx)$/,
-        exclude: /nodeModules/,
+        exclude: /node_modules/,
         use: {
           loader: "babel-loader",
         },
